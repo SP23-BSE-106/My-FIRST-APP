6c2603a76f30a5510c564bf12442e0c57b11da2b
SP23-BSE-106 <munazzajaved277@gmail.com>
2024-12-06 22:00:07 +0500

    My First App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Fun from './Fun.js';
+
+function App() {
+  return (
+   <Fun/>
+  );
+}
+
+export default App;
---
+++ src/Fun.js
@@ -0,0 +1,4 @@
+function Fun(){
+    return (<h1>Allah is One</h1>);
+}
+export default Fun;
